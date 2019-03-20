1b4c09c9113ddbb2cb383677f669d4a5fe07962f
妙蛙种子 <zjl198544@163.com>
2019-03-20 08:30:35 +0800

    🐛 修复java项目里的ng无法打包问题

--- src/java/__name@dasherize__-ng/src/app/routes/my-module.module.ts
+++ src/java/__name@dasherize__-ng/src/app/routes/my-module.module.ts
@@ -2,29 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { CommonModule } from "@angular/common";
 
-import { UsermanagerModule } from './usermanager/usermanager.module';
-import { SystemparameterModule } from './systemparameter/systemparameter.module';
-import { UnitModule } from './unit/unit.module';
-import { RolesModule } from './roles/roles.module';
-import { ResourcesModule } from './resources/resources.module';
-import { UserModule } from './user/user.module';
-import { OrganizationModule } from './organization/organization.module';
-import { LogModule } from './log/log.module';
-import { DictionaryModule } from './dictionary/dictionary.module';
-import { CustomerModule } from './customer/customer.module';
-
-const MYMODULES = [
-  UsermanagerModule,
-  SystemparameterModule,
-  UnitModule,
-  RolesModule,
-  ResourcesModule,
-  UserModule,
-  OrganizationModule,
-  LogModule,
-  DictionaryModule,
-  CustomerModule
-];
+const MYMODULES = [];
 
 @NgModule({
   imports: [
