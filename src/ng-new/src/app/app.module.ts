5ba07e314866317eff308efba28b1bf1773854a7
妙蛙种子 <zjl198544@163.com>
2019-06-12 07:54:48 +0800

    💄 升级依赖删除其他

--- src/ng-new/src/app/app.module.ts
+++ src/ng-new/src/app/app.module.ts
@@ -15,7 +15,6 @@ import { RoutesModule } from './routes/routes.module';
 import { LayoutModule } from './layout/layout.module';
 import { StartupService } from '@core/startup/startup.service';
 import { DefaultInterceptor } from '@core/net/default.interceptor';
-import { SimpleInterceptor } from '@delon/auth';
 // angular i18n
 import { registerLocaleData } from '@angular/common';
 import localeZh from '@angular/common/locales/zh';
@@ -79,7 +78,6 @@ export function StartupServiceFactory(
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'zh-Hans' },
-    { provide: HTTP_INTERCEPTORS, useClass: SimpleInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true },
     { provide: ALAIN_I18N_TOKEN, useClass: I18NService, multi: false },
     StartupService,
